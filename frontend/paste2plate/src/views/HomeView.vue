e6aec67f1f54329f6b3b5202aed183b09ac1dcd7
Robin Ferm <robinferm@live.com>
2024-12-02 22:08:25 +0100

    init frontend

---
+++ frontend/paste2plate/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <div class="grid lg:grid-cols-3 gap-4">
+      <RecipeCard></RecipeCard>
+      <RecipeCard></RecipeCard>
+      <RecipeCard></RecipeCard>
+      <RecipeCard></RecipeCard>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import RecipeCard from '@/components/RecipeCard.vue'
+</script>
