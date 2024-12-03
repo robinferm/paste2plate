3e0fbdec433b33185e6c40d6029a1b839f1a4a72
Robin Ferm <robinferm@live.com>
2024-12-03 22:36:55 +0100

    add and delete recipe

--- frontend/paste2plate/src/api/api.ts
+++ frontend/paste2plate/src/api/api.ts
@@ -11,3 +11,22 @@ export const getRecipe = async (id: number): Promise<Recipe> => {
   const recipe: Recipe = await res.json()
   return recipe
 }
+
+export const deleteRecipe = async (id: number) => {
+  await fetch(`http://localhost:3000/recipes/${id}`, {
+    method: 'DELETE',
+  })
+}
+
+export const addRecipe = async (recipe: Recipe): Promise<Recipe> => {
+  const res = await fetch('http://localhost:3000/recipes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(recipe),
+  })
+
+  const addedRecipe: Recipe = await res.json()
+  return addedRecipe
+}
--- frontend/paste2plate/src/views/RecipeList.vue
+++ frontend/paste2plate/src/views/RecipeList.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <button class="btn" @click="addRecipeClick()">Add</button>
     <div class="grid lg:grid-cols-3 gap-4">
       <RecipeCard
         v-for="recipe in recipes"
@@ -17,6 +18,7 @@ import RecipeCard from '@/components/RecipeCard.vue'
 import router from '@/router'
 import type { Recipe } from '@/types/recipe'
 import { onMounted, ref } from 'vue'
+import { addRecipe } from '@/api/api'
 
 const recipes = ref<Recipe[]>([])
 onMounted(async () => {
@@ -27,5 +29,26 @@ onMounted(async () => {
 
 const openRecipe = (id: number) => {
   router.push({ name: 'RecipeDetail', params: { id } })
+}
+
+const addRecipeClick = async () => {
+  const newRecipe: Recipe = {
+    id: 6,
+    title: 'Tomato Soup',
+    ingredients: [
+      { name: 'Tomatoes', amount: '500 g' },
+      { name: 'Onion', amount: '1' },
+      { name: 'Garlic', amount: '2 cloves' },
+    ],
+    instructions: [
+      'Chop the onions and garlic.',
+      'Saute them in a pot.',
+      'Add tomatoes and cook for 20 minutes.',
+      'Blend and serve.',
+    ],
+    image: 'https://img.koket.se/standard-giant/snabb-pasta-och-tomatsas-med-burrata.jpg.webp',
+  }
+  recipes.value.push(newRecipe)
+  await addRecipe(newRecipe)
 }
 </script>
